7ba6a51a9e93dd144e4e0d7abec7def06dcd22f1
Gerard Roche <gerardroche@users.noreply.github.com>
2023-07-21 18:57:58 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,6 +1,70 @@
 # Toggler
 
-Toggle settings.
+Toggle settings command with on/off configurations.
+
+## Commands
+
+Command                     | Description
+:------                     | :----------
+Toggle Fold Buttons         | Toggle `fold_buttons`
+Toggle Highlight Line       | Toggle `highlight_line`
+Toggle Indent Guides        | Toggle `indent_guide_options`; on: `["draw_normal", "draw_active"]`, off: `[]`
+Toggle Invisibles           | Toggle `draw_white_space`; on: `"all"`, off: `"selection"`
+Toggle Line Numbers         | Toggle `line_numbers`
+Toggle Preview on Click     | Toggle `preview_on_click`
+Toggle Rulers               | Toggle `rulers`; on: `[80, 120]`, off: `[]`
+Toggle Save on Focus Lost   | Toggle `save_on_focus_lost`
+
+## Settings
+
+User settings can override the default on and off values for complex settings by appending `"_toggle_on"` and `"_toggle_off"` to the setting name.
+
+For instance to customize the "off" value for Invisibles:
+
+```js
+"draw_white_space_toggle_off":
+[
+    "leading_mixed",
+    "selection",
+    "trailing",
+    "isolated"
+],
+```
+
+Another example customizing the "on" value for Indent Guides:
+
+```
+"indent_guide_options_toggle_on":
+[
+    "draw_normal",
+    "solid",
+    "draw_active"
+],
+```
+
+## Custom Commands
+
+Command name: `toggler`
+
+Argument  | Type     | Default  | Description
+:-------- | :------- | :------- | :----------
+`setting` | `string` |          | Setting name
+`on`      |          | `true`   | Default on value
+`off`     |          | `false`  | Default off value
+
+**Example**
+
+```json
+{
+    "caption": "Toggle Indent Guides",
+    "command": "toggler",
+    "args": {
+        "setting": "indent_guide_options",
+        "on": ["draw_normal", "draw_active"],
+        "off": []
+    }
+}
+```
 
 ## License
 
---
+++ plugin.py
@@ -0,0 +1,57 @@
+from contextlib import contextmanager
+
+from sublime import load_settings
+from sublime import save_settings
+from sublime import status_message
+from sublime import windows
+import sublime_plugin
+
+
+class Toggler(sublime_plugin.WindowCommand):
+
+    def run(self, setting=None, **kwargs):
+        self.setting = setting
+
+        on = kwargs.get('on') if 'on' in kwargs else True
+        off = kwargs.get('off') if 'off' in kwargs else False
+
+        with save_preferences() as preferences:
+            preference_name = setting if setting else self.setting
+
+            value = self.get_on_value_or(on)
+            if preferences.get(preference_name) == value:
+                value = self.get_off_value_or(off)
+
+            print('value =', value)
+            preferences.set(preference_name, value)
+
+            for window in windows():
+                for view in window.views():
+                    view.settings().erase(preference_name)
+
+        status_message('{} is {}'.format(
+            self.setting.replace('_', ' ').title(),
+            'enabled' if value == self.get_on_value_or(on) else 'disabled'
+        ))
+
+    def get_off_value_or(self, default):
+        return self.get_setting('%s_toggle_off' % self.setting, default)
+
+    def get_on_value_or(self, default):
+        return self.get_setting('%s_toggle_on' % self.setting, default)
+
+    def get_setting(self, name: str, default=None):
+        view = self.window.active_view()
+        if view:
+            value = view.settings().get(name)
+            if value:
+                return value
+
+        if default is not None:
+            return default
+
+
+@contextmanager
+def save_preferences():
+    yield load_settings('Preferences.sublime-settings')
+    save_settings('Preferences.sublime-settings')
